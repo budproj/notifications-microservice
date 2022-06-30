18c243f0001b324447b1219ab145604ba02aa6fd
Victor Perin <me@victorperin.ninja>
2022-06-30 17:38:22 -0300

    refactor(src): remove unused methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsGateway } from './app.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, EventsGateway],
+  controllers: [],
+  providers: [EventsGateway],
 })
 export class AppModule {}
