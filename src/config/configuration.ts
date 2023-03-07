39734ee02d48e0ccdcebb47fadb21a25238e31fa
Victor Perin <me@victorperin.ninja>
2023-03-07 12:35:09 -0300

    refactor(configuration): using rabbitmq connection string

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
-  natsConnectionString:
-    process.env.NATS_CONNECTION_STRING || 'nats://localhost:4222',
+  rabbitmqConnectionString:
+    process.env.RABBITMQ_CONNECTION_STRING || 'amqp://rabbitmq:5672',
 });
